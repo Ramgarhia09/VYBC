5a230917b87d16819019ce356b1f25fcc4a0208a
Ramgarhia09 <141401014+Ramgarhia09@users.noreply.github.com>
2025-10-30 15:43:56 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
+
 const {
   addProduct,
   getProducts,
   updateStock,
   getCategories,
+  getProductStats,
 } = require("../controllers/productController");
 
-// ✅ Do NOT write "/products" again
 router.post("/", protect, addProduct);
-router.get("/", protect, getProducts);
+router.get("/", protect, getProducts);   // ?filter=active,new,low,out
 router.put("/stock/update/:id", protect, updateStock);
 router.get("/categories", protect, getCategories);
+router.get("/stats", protect, getProductStats);
 
 module.exports = router;
