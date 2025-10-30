895b461fd6ff83ce117e92e42748613040f4b815
Ramgarhia09 <141401014+Ramgarhia09@users.noreply.github.com>
2025-10-30 09:50:15 +0530

    Update invoiceRoutes.js

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -12,5 +12,6 @@ router.post("/invoices", createInvoice);           // 4.1
 router.get("/invoices", getInvoices);              // 4.2
 router.put("/invoices/:id", updateInvoice);        // 4.3
 router.post("/invoices/:id/share", shareInvoice);  // 4.4
-
+router.delete("/invoices/:id", protect, deleteInvoice);
 module.exports = router;
+
